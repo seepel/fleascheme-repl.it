7baf7630389d8a98a953962805bb0c357fc2742b
Sean Lynch <sean@seanplynch.com>
2020-08-29 19:26:16 -0700

    Almost meta-circular

--- main.scm
+++ main.scm
@@ -4,9 +4,9 @@
 (define-macro (test result expected)
   (if tests
     `(let ((result ,result)
-           (expected ,expected)) 
+           (expected ,expected))
        (if (equal? ,result ,expected)
-         (begin 
+         (begin
            (display (string-append "Test Succeeded\n"))
            (print (quote ,result)))
          (begin
@@ -51,219 +51,285 @@
 (test (dlet* (((a b c) '(1 2 3))) b) 2)
 (test (dlet* (((a b c) '(1 2 3))) c) 3)
 
-
-(define (assert x . messages)
-  (unless x
-    (raise (string-append messages))))
+(define modulo mod)
 
 (define (error message . args)
-  ;; FIXME: Handle formats (~a ~s . rest)
-  (raise message))
- 
-(define-macro (free x) `(set! ,x #f))
+  (let ((output (open-output-string)))
+   (display message output)
+   (for-each (lambda (x) (write x output))
+	     args))
+   (raise (get-output-string output)))
 
-(define-macro (mif be te ee)
-   (let ((vs 
-         ;;(car (freevars be '() '()))
-         ))
-     `(if-function (lambda () ,be)
-        (lambda ,vs ,te)
-        (lambda ,vs ,ee))))
- 
-(define (if-function be te ee)
-  (dlet* (((pval . stuff) (be)))
-    (if pval 
-      (apply te stuff)
-      (apply ee stuff))))
+(define lambda-parameters cadr)
+(define lambda-body cddr)
 
-(define-macro (mcond . clauses)
-  (dlet* ((((be . rest) . clauses) clauses))
-    (if (eq? be 'else)
-      `(begin ,@rest)
-      `(mif ,be 
-         (begin ,@rest)
-         (mcond ,@clauses)))))
-
-(define (mcopy x)
-  "Pedagogical non-primitive copy function."
-  (mif (matom? x) 
-    `(,x ,@x)
-    (dlet* (((carx . cdrx) x)
-            ((ncar1 . ncar2) (mcopy carx))
-            ((ncdr1 . ncdr2) (mcopy cdrx)))
-      `((,ncar1 ,@ncdr1) . (,ncar2 ,@ncdr2)))))
-
-(define (matom? x)
-  ;; FIXME: Other types?
-  `(,(or (symbol? x)
-         (procedure? x)
-         (number? x)
-         (string? x)
-         (null? x))
-    ,x)) 
-
-(define (meq? x y) `(,(eq? x y) ,x))
-
-(define (meq2? x y) `(,(eq? x y) ,x ,y))
-
-(define (msymbol? x) `(,(symbol? x) ,x))
-
-(define (mnull? x) `((,null? x) ,x))
-
-(define (massoc x e)
-  "return binding, else #f"
-  (mif (mnull? e)
-    (begin
-      (free x)
-      `(#f ,@e))
-    (dlet* ((((var . val) . reste) e))
-      (mif (meq2? x var)
-        (begin
-          (free x)
-          `((,var ,@val) ,@reste))
-        (dlet* (((nbinding . nreste) (massoc x reste)))
-          `(,nbinding . ((,var ,@val) ,@reste)))))))
-
-(define (meval-begin xl e)
-  (dlet* (((x . xl) xl))
-    (mif (mnull? xl)
-      (begin
-        (assert (null? xl))
-        (meval x e))
-      (dlet* (((xval . ne) (meval x e))
-              ((xlval . nne) (meval-begin xl ne)))
-        (free xval)
-        `(,xlval ,@nne)))))
-
-(define (meval x e)
-  (mcond
-    ((msymbol? x)
-     (dlet* ((((var .val) . ne) (massoc x e)))
-       (free var)
-       `(,val ,@ne)))
-    ((matom? x) 
-     `(,x ,@e))
-    (else ; better be a pair
-     (dlet* (((fn . args) x)) ; oh hey, there was a typo here in the paper (missing begin paren in the bindings)
-       (mcond
-         ((meq? fn 'begin)
-          (free fn)
-          (meval-begin args e))
-         ((meq? fn 'function)
-          (free fn)
-          (dlet* (((lambda) args)
-                  ((nlambda . lambda) 
-                   (mcopy lambda))
-                  ((fvars . bvars) 
-                   (freevars `(lambda ,nlambda) '() '()))
-                  ((e1 . e2) (split fvars e)))
-           ;; TODO: decide on funarg vs closure
-           `((funarg ,lambda ,e1) ,@e2)))
-         (else ; procedure application
-          ;; This is different because scheme is a lisp-1 and linear lisp is a lisp-2
-          (dlet* ((nfn . ne) (meval fn e)
-                  ((nargs . nne) (mevlis args( ne)))
-            `(,(mapply nfn nargs ,@nne))))))))))
-
-(define (mapply fn args)
-  (mif (matom? fn) ; FIXME: this means it's a host procedure
-    (apply fn args)
-    (dlet* (((ffn . rfn) fn))
-      (mcond
-        ((meq? ffn 'function)
-         (free ffn)
-         (dlet* (((bvlist . body) rfn)
-                 ((v . ne) (meval-begin body (mpairlis bvlist args '()))))
-           (assert (null? ne))
-           v))
-        ((meq? ffn 'funarg)
-         (free ffn)
-         (dlet* ((((lambda bvlist . body) ce) rfn)
-                 ((v . ne) (meval-begin body (mpairlis bvlist args ce))))
-           (free lambda)
-           (assert (null? ne)
-           v))
-        (else
-         (error "mapply: bad fn ~S" fn)))))))
-
-(define (mpairlis vars vals e)
-  (mif (mnull? vars)
-    (begin
-      (assert (null? vars))
-      e)
-    (dlet* (((var . vars) vars)
-            ((val . vals) vals))
-      `(,var ,@val) ,@(mpairlis vars vals e))))
-
-(define (mevlis args e)
-  (mif (mnull? args)
-    (begin
-      (assert (null? args))
-      `(() ,@e))
-    (dlet* (((x . args) args)
-            ((xval . e) (meval x e))
-            ((argvals . e) (mevlis args e)))
-      `((,xval ,@argvals) ,@e))))
-
-(define (split vars e)
-  (mif (mnull? args)
-    (begin
-      (assert (null? vars)
-      (`() ,@e)))
-    (dlet* (((var . nvars) vars)
-            ((binding . ne) (massoc var e))
-            ((e1 . e2) (split nvars ne)))
-      `((,binding ,@e1) ,@e2))))
-
-(define (mmember? x ls)
-  (mif (mnull? ls)
-    (begin
-      (free x)
-      `(() ,@ls))
-    (dlet* (((carls . ls) ls))
-      (mif (meq2? x carls)
-        (begin
-          (free x)
-          `(,carls ,@ls))
-        (dlet* (((tval . rest) (mmember? x ls)))
-          `(,tval . (,carls ,@rest)))))))
-
-(define (freelistvars xl bvars fvars)
-  (if (null? xl)
-    (begin
-      (assert (null? xl))
-      `(,fvars ,@bvars))
-    (dlet* (((x . xl) xl)
-            ((nfvars . nbvars) (freelistvars xl bvars fvars)))
-      (freevars x nbvars nfvars))))
-
-(define (freevars x bvars fvars)
-  "return new fvars and new bvars"
-  (mcond
-    ((msymbol? x)
-     (dlet* (((x1 . x2) (mcopy x))
-             ((x2 . x3) (mcopy x2))
-             ((p1val . nbvars) (mmember? x1 bvars))
-             ((p2val . nfvars) (mmember? x2 fvars)))
-       (mif p1val
-         (begin
-           (free x3)
-           `(,nfvars ,@nbvars)i)
-         (mif p2val
-           (begin
-             (free x3)
-             `(,nfvars ,@nbvars))
-           `((,x ,@nfvars) ,@nbvars)))))
-    ((matom? x)
-     (free x)
-     `(,fvars ,@bvars))
+(define (bound-identifier? identifier bound-identifiers)
+  (cond 
+    ((null? bound-identifiers)
+     (values #f bound-identifiers))
+    ((eq? identifier (car bound-identifiers))
+     (values #t (cdr bound-identifiers)))
     (else
-     (dlet* (((fn . args) x))
-       (mcond
-         ((meq? fn 'function)
-          (free fn)
-          (dlet* ((((lambda bvlist . body)) args))
-            (free lambda)
-            (freelistvars body `(,@bvlist ,@bvars) fvars)))
-         (else
-          (freelistvars `(,fn ,@args) bvars fvars)))))))
+      (let-values (((true? remaining-bound-identifiers)
+                    (bound-identifier? identifier (cdr bound-identifiers)))) 
+       (values true?
+               (cons (car bound-identifiers)
+                     remaining-bound-identifiers))))))
+(define (free-variables expression bound-identifiers environment)
+  (cond
+    ((self-quoting? expression)
+     (values '() bound-identifiers environment))
+    ((identifier? expression)
+     (let-values (((bound? bound-identifiers)
+                   (bound-identifier? expression bound-identifiers)))
+       (if bound?
+           (values '() bound-identifiers environment)
+           (let-values (((binding environment) (environment-ref environment expression)))
+             (if binding
+                 (values (list binding) bound-identifiers environment)
+                 (begin
+                   (display "Error: Unbound identifier: ")
+                   (display expression)
+                   (newline)
+                   ;; error though
+                   (values '() bound-identifiers environment)))))))   
+    ((null? expression)
+     (values '() bound-identifiers environment))
+    ((pair? expression)
+     (case (first expression)
+       ((quote)
+        (values '() bound-identifiers environment))
+       ((lambda)
+        (free-variables (lambda-body expression) 
+                        (append (lambda-parameters expression) bound-identifiers)
+                        environment))
+       ((begin)
+        (free-variables (cdr expression) bound-identifiers environment))
+       ((dup)
+        (let ((binding (assv (second expression) environment)))
+          (if binding
+              (values (list binding) bound-identifiers environment)
+              (begin
+                (display "Error: Unbound identifier: ")
+                (display expression)
+                (newline)
+                ;; error though
+                (values '() bound-identifiers environment)))))
+       (else 
+        (let*-values (((a d)
+                            (car+cdr expression))
+                      ((a-free-variables bound-identifiers environment)
+                       (free-variables a bound-identifiers environment))
+                      ((d-free-variables bound-identifiers environment) 
+                       (free-variables d bound-identifiers environment)))
+          (values (append a-free-variables d-free-variables) bound-identifiers environment)))))))
+
+(define (primitive-procedure? x)
+  (values (procedure? x) x))
+
+(define (compound-procedure? x)
+  (values (eqv? (car x) 'compound-procedure) x))
+
+(define (make-procedure parameters body environment)
+  (list 'compound-procedure parameters body environment))
+
+(define (feval-lambda parameters body environment)
+  (let*-values (((procedure-environment _ environment) (free-variables body parameters environment)))
+    (values (make-procedure parameters body procedure-environment) environment)))
+
+(define (procedure-parameters procedure)
+  (cadr procedure))
+
+(define (procedure-body procedure)
+  (caddr procedure))
+
+(define (procedure-environment procedure)
+  (cadddr procedure))
+
+(define (acons key value alist)
+  (cons (cons key value) alist))
+
+(define empty-environment 
+  `((+ . ,+) (- . ,-) (* . ,*) (/ . ,/) (modulo . ,modulo)
+    (cons . ,cons) (car . ,car) (cdr . ,cdr) (car+cdr . ,car+cdr)
+    (> . ,>) (< . ,<) (>= . ,>=) (<= . ,<=) (= . ,=)
+    (eq? . ,eq?) (eqv? . ,eqv?) (equal? . ,equal?)
+    (display . ,display) (write . ,write) (newline . ,newline)
+    (values . ,values)
+    ))
+
+(define (extend-environment environment bindings values)
+  (cond 
+    ((null? bindings)
+     environment)
+    ((null? values)
+     (extend-environment (acons (car bindings) #f environment)
+                         (cdr bindings)
+                         values))
+    (else
+      (extend-environment (acons (car bindings) (car values) environment)
+                          (cdr bindings)
+                          (cdr values)))))
+
+(define (environment-ref environment identifier)
+  (if (null? environment)
+      (values #f environment)
+      (let-values (((binding environment) (car+cdr environment)))
+        (if (eqv? (car binding) identifier)
+            (if (primitive-procedure? (cdr binding))
+              (values binding (cons binding environment))
+              (values binding environment))
+            (let-values (((found-binding environment) 
+                          (environment-ref environment identifier)))
+              (values found-binding (cons binding environment)))))))
+
+(define (self-quoting? x)
+  (values (or (number? x)
+              (boolean? x)
+              (string? x)) 
+          x))
+
+(define (identifier? x)
+  (values (symbol? x) x))
+
+(define (feval-identifier identifier environment)
+  (let-values (((binding environment) (environment-ref environment identifier)))
+    (if binding
+        (values (cdr binding) environment)
+        (error "Unbound identifier: " identifier))))
+
+(define (application? x)
+  (values (pair? x) x))
+
+(define (feval-operands operands environment)
+  (if (null? operands)
+      (values operands environment)
+      (let*-values (((operand environment) (feval (car operands) environment))
+                    ((rest environment) (feval-operands (cdr operands) environment)))
+        (values (cons operand rest) environment))))
+
+(define (feval-application expression environment)
+  (let*-values (((operator environment)
+                 (feval (car expression) environment))
+                ((operands environment)
+                 (feval-operands (cdr expression) environment)))
+    (values (fapply operator operands) environment)))
+
+(define unspecified '*unspecified*)
+
+(define (feval-sequence expressions environment)
+  (cond
+    ((null? expressions) 'unspecified)
+    ((null? (cdr expressions)) (feval (car expressions) environment))
+    (else (let-values (((value environment)
+                        (feval (car expressions) environment)))
+            (feval-sequence (cdr expressions) environment)))))
+
+(define (feval-define expression environment)
+  (if (pair? (cadr expression))
+      (feval `(define ,(caadr expression)
+                (lambda ,(cdadr expression)
+                  ,@(cddr expression)))
+             environment)
+      (let*-values (((identifier) (cadr expression))
+                    ((self-binding) (cons identifier '*self*))
+                    ((environment) (if (and (pair? (caddr expression))
+                                            (eqv? (caaddr expression) 'lambda))
+                         (cons self-binding environment)
+                         environment))
+                    ((value environment) (feval (caddr expression) environment)))
+
+            (display environment)
+            (newline)
+        (when (and (pair? environment)
+                   (eqv? self-binding (car environment)))
+          (set! environment (cdr environment)))
+        (values unspecified (acons identifier value environment)))))
+
+(define (feval-dup identifier environment)
+  (if (symbol? identifier)
+      (let ((binding (assv identifier environment)))
+        (if binding
+            (values (cdr binding) environment)
+            (error "Error: dup: Unobound identifier: " identifier)))
+      (error "Error: dup: Expected an identifier but got: " identifier)))
+
+(define (feval expression environment)
+  (cond
+    ((self-quoting? expression)
+     (values expression environment))
+    ((identifier? expression) 
+     (feval-identifier expression environment))
+    ((application? expression)
+     (case (car expression)
+       ((dup)
+        (feval-dup (second expression) environment))
+       ((lambda)
+        (feval-lambda (cadr expression) (cddr expression) environment))
+       ((quote) 
+        (values (cadr expression) environment))
+       ((define)
+        (feval-define expression environment))
+       ((begin)
+        (feval-sequence (cdr expression) environment))
+       ((if)
+        (let*-values (((true? environment) (feval (second expression) environment)))
+          (if true?
+              (feval (third expression) environment)
+              (feval (fourth expression) environment))))
+       (else (feval-application expression environment) )))))
+
+(define (apply-primitive-procedure procedure arguments)
+  (apply procedure arguments))
+
+(define (unused-bindings environment)
+  (if (null? environment)
+      '()
+      (dlet* ((((identifier . value) . environment) environment))
+        (if (primitive-procedure? value)
+            (unused-bindings environment)
+            (acons identifier value (unused-bindings environment))))))
+
+(define (fapply operator operands)
+  (cond 
+    ((primitive-procedure? operator)
+     (apply-primitive-procedure operator operands))
+    ((compound-procedure? operator)
+     (let*-values (((environment)
+                    (map (lambda (binding)
+                           (if (eqv? (cdr binding) '*self*)
+                             (cons (car binding) operator)
+                             binding))
+                         (extend-environment (procedure-environment operator)
+                                             (procedure-parameters operator)
+                                                           operands)))
+                   ((value environment) (feval-sequence (procedure-body operator) environment))
+                   ((unused-bindings) (unused-bindings environment)))
+       (if (null? unused-bindings) 
+           (values value environment)
+           (error "Unused bindings:" unused-bindings))))
+    (else (error "Wrong type to apply: ~a" operator))))
+
+(define (repl environment)
+  (display "* ")
+  (let*-values ((input (read))
+                ((output environment) (feval input environment)))
+    (display "=> ")
+    (display output)
+    (newline)
+    (repl environment)))
+
+(define fizzbuzz
+  '(define (fizzbuzz x y)
+     (display
+       (if (= (modulo (dup x) 15) 0)
+           "FizzBuzz"
+           (if (= (modulo (dup x) 3) 0)
+             "Fizz"
+             (if (= (modulo (dup x) 5) 0)
+               "Buzz"
+               (dup x)))))
+     (newline)
+     (if (< (dup x) (dup y))
+         (fizzbuzz (+ x 1) y)
+         (begin x y fizzbuzz))))
